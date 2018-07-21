2b19f61a4d0e0d57dc2628f1267b87dbaece1fd0
Jong Seok Won <jwon@redhat.com>
2018-07-21 16:11:04 +0900

    add example code

---
+++ hello-kafka-1.1.1/src/main/java/org/jbugkorea/kafka/ConsumerExample.java
@@ -0,0 +1,27 @@
+package org.jbugkorea.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("headers = %s, partition = %d, offset = %d, key = %s, value = %s%n", record.headers(), record.partition(), record.offset(), record.key(), record.value());
+        }
+    }
+}
